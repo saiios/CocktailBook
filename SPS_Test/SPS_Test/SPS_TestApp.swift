4f4618c226548e2729b22ed23ce6c41d89572eb3
APPLE <>
2024-12-13 08:33:22 +0530

    initial commit

---
+++ SPS_Test/SPS_Test/SPS_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SPS_TestApp.swift
+//  SPS_Test
+//
+//  Created by APPLE on 13/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SPS_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
